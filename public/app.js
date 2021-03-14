7672681c9926e74c132c1cf1de6ebbf386dd6b3a
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-03-14 22:41:13 +0530

    2 video bug fixed

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const socket = io();
 const videoGrid = document.getElementById("video-grid");
 const ROOM_ID = document.getElementById("body").dataset.room;
+const peers = {};
 
 const myPeer = new Peer(undefined, {
   host: "/",
@@ -15,13 +16,13 @@ navigator.mediaDevices
   .then((stream) => {
     addVideoStream(stream);
     socket.on("user-connected", (userID) => {
-      console.log("USER connected:", userID);
       addNewUser(userID, stream);
     });
     myPeer.on("call", (call) => {
       call.answer(stream);
+      const customVideo = document.createElement("video");
       call.on("stream", (userVideoStream) => {
-        addVideoStream(userVideoStream);
+        addVideoStream(userVideoStream, customVideo);
       });
     });
   });
@@ -30,24 +31,33 @@ myPeer.on("open", (userID) => {
   socket.emit("join-room", ROOM_ID, userID);
 });
 
-socket.on("user-disconnected", (userID) => {
-  console.log("USER disconnected:", userID);
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
 });
 
-function addVideoStream(stream) {
-  const video = document.createElement("video");
+function addVideoStream(stream, video) {
+  if (!video) {
+    video = document.createElement("video");
+  }
   video.muted = true;
   video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-  });
+  video.autoplay = true;
+  // video.addEventListener("loadedmetadata", () => {
+  //   video.play();
+  // });
   videoGrid.append(video);
 }
 
 function addNewUser(userID, stream) {
   const call = myPeer.call(userID, stream);
+  const customVideo = document.createElement("video");
+  // add custom video element before stream event otherwise it receives two events
   call.on("stream", (userVideoStream) => {
-    addVideoStream(userVideoStream);
+    addVideoStream(userVideoStream, customVideo);
   });
-  // call.on("close")
+  call.on("close", () => {
+    customVideo.remove();
+  });
+
+  peers[userID] = call;
 }
